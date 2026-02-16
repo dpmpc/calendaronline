ab630bc4de96685fe300898ee1d308dd1bbc9eda
dpmpc <145990621+dpmpc@users.noreply.github.com>
2026-02-16 08:27:33 +0100

    Show preview as image in a modal popup window (#56)

--- web/creator/views.py
+++ web/creator/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from creator.fotocalendar.bo.config import CalendarConfig
 from creator.fotocalendar.creator import get_default_config_for_format, get_default_config_for_request, get_config_for_request, create_from_config, create_preview_from_request
+from pdf2image import convert_from_bytes
+from pdf2image.exceptions import PDFInfoNotInstalledError, PDFPageCountError, PDFSyntaxError, PopplerNotInstalledError
 
 
 def index(request):
@@ -24,7 +26,7 @@ def month(request):
 def load(request):
     content = request.FILES.get('file')
     config = CalendarConfig.loads(content.read())
-    
+
     template = loader.get_template('creator/months.html')
     return HttpResponse(template.render(config.asdict(), request))
 
@@ -44,7 +46,19 @@ def create(request):
 
 def preview(request):
     calendar = create_preview_from_request(request)
-    return HttpResponse(calendar.output(), content_type="application/pdf")
+    pdf_output = calendar.output()
+    if request.GET.get('pdf', '0') == '1':
+        return HttpResponse(pdf_output, content_type="application/pdf")
+    else:
+        try:
+            pages = convert_from_bytes(pdf_output, dpi=72, first_page=1, last_page=1)
+            img = pages[0]
+            response = HttpResponse(content_type="image/png")
+            img.save(response, "PNG")
+            return response
+        except (PDFInfoNotInstalledError, PopplerNotInstalledError, PDFPageCountError, PDFSyntaxError, IndexError):
+            # PDF conversion failed - fall back to PDF output
+            return HttpResponse(pdf_output, content_type="application/pdf")
 
 
 def faq(request):
